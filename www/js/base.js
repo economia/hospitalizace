35ba4468b9844845db476161e11ad85fc0b4a833
Marcel Šulek <marcel@sulek.eu>
2013-08-14 10:43:07 +0200

    nacitani hodnot hospitalizace.csv

--- www/js/base.js
+++ www/js/base.js
@@ -1,3 +1,25 @@
 (function(){
+  var loadHospitalizace, this$ = this;
   d3.selectAll(".fallback").remove();
+  loadHospitalizace = function(cb){
+    var ssv, this$ = this;
+    ssv = d3.dsv(";", "text/csv");
+    return ssv("../hospitalizace.csv", function(row){
+      return {
+        kod: row.KOD,
+        rok: row.ROK,
+        pohlavi: row.POHL === "1" ? "muz" : "zena",
+        vek: row.VEKKAT,
+        kraj: +row.KRAJ,
+        hosp: +row.HOSP
+      };
+    }, function(err, data){
+      return cb(err, data);
+    });
+  };
+  async.parallel([loadHospitalizace], function(err, arg$){
+    var hospitalizace;
+    hospitalizace = arg$[0];
+    return console.log(hospitalizace);
+  });
 }).call(this);
--- www/ls/base.ls
+++ www/ls/base.ls
@@ -1,1 +1,17 @@
 d3.selectAll ".fallback" .remove!
+loadHospitalizace = (cb) ->
+    ssv = d3.dsv ";" "text/csv"
+    (err, data) <~ ssv do
+        "../hospitalizace.csv"
+        (row) ->
+            kod: row.KOD
+            rok: row.ROK
+            pohlavi: if row.POHL == "1" then "muz" else "zena"
+            vek: row.VEKKAT
+            kraj: +row.KRAJ
+            hosp: +row.HOSP
+    cb err, data
+
+(err, [hospitalizace]) <~ async.parallel [loadHospitalizace]
+console.log hospitalizace
+
